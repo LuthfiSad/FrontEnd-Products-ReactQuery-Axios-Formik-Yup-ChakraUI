f6862ef39eabdb3e877990142c96a09806429216
muhamad luthfi sadli <luthfisad1806@gmail.com>
2024-06-03 14:47:27 +0700

    first commit

--- README.md
+++ README.md
@@ -1,40 +1,54 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# FrontEnd-Products-ReactQuery-Axios-Formik-Yup-ChakraUI
 
-## Getting Started
+Proyek ini adalah aplikasi frontend yang dibangun dengan Next.js, React Query, Axios, Formik, Yup, dan Chakra UI. API backend tersedia di [API-Products-ExpressJS-Postgres-Prisma](https://github.com/LuthfiSad/API-Products-ExpressJS-Postgres-Prisma).
 
-First, run the development server:
+## Daftar Isi
+
+- [Scripts](#scripts)
+- [Teknologi](#teknologi)
+- [Penggunaan](#penggunaan)
+
+## Scripts
+
+Script berikut tersedia dalam proyek ini:
+
+- **dev**: Memulai server pengembangan.
+- **build**: Membangun aplikasi untuk produksi.
+- **start**: Memulai aplikasi dalam mode produksi.
+- **lint**: Melakukan linting pada kode.
+
+Anda bisa menjalankan script ini menggunakan npm:
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
+npm run dev    # Memulai server pengembangan
+npm run build  # Membangun aplikasi untuk produksi
+npm run start  # Memulai aplikasi dalam mode produksi
+npm run lint   # Melakukan linting pada kode
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Teknologi
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+Proyek ini menggunakan teknologi berikut:
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
+- **Next.js**: Framework React untuk server-side rendering dan static site generation.
+- **React Query**: Perpustakaan pengambilan data untuk React.
+- **Axios**: Klien HTTP berbasis promise untuk browser dan Node.js.
+- **Formik**: Perpustakaan untuk membangun form di React.
+- **Yup**: Builder skema JavaScript untuk parsing dan validasi nilai.
+- **Chakra UI**: Perpustakaan komponen yang sederhana, modular, dan aksesibel untuk React.
 
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
+## Penggunaan
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+Untuk menggunakan aplikasi ini, ikuti langkah-langkah berikut:
 
-## Learn More
+1. Memulai server pengembangan:
 
-To learn more about Next.js, take a look at the following resources:
+    ```bash
+    npm run dev
+    ```
 
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
+2. Buka browser Anda dan navigasikan ke `http://localhost:3000`.
 
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+3. Interaksi dengan aplikasi sesuai kebutuhan.
 
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+Pastikan API backend berjalan dengan mengikuti petunjuk di repositori [API-Products-ExpressJS-Postgres-Prisma](https://github.com/LuthfiSad/API-Products-ExpressJS-Postgres-Prisma).
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,159 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import {
+  Container,
+  Heading,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+  Spinner,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+  Button,
+  useToast,
+  HStack,
+  TableContainer,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {
+  useCreateProduct,
+  useDeleteProduct,
+  useEditProduct,
+  useFetchProducts,
+} from "@/features/product";
 
-const inter = Inter({ subsets: ["latin"] });
+// formik -> handle forms
+// yup -> validate
+// react-query -> manage API calls (caching, state, dll.)
 
 export default function Home() {
+  const toast = useToast();
+
+  const {
+    data,
+    isLoading: productsIsLoading,
+    refetch: refetchProducts,
+  } = useFetchProducts({
+    onError: () => {
+      toast({
+        title: "Ada kesalahan terjadi",
+        status: "error",
+      });
+    },
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      price: "",
+      description: "",
+      image: "",
+      id: "",
+    },
+    onSubmit: async () => {
+      const { name, description, image, id } = formik.values;
+      const price = parseInt(formik.values.price) | 0;
+
+      if (id) {
+        // Melakukan PATCH /products/{id}
+        editProduct({
+          name,
+          price,
+          description,
+          image,
+          id,
+        });
+
+        toast({
+          title: "Product edited",
+          status: "success",
+        });
+      } else {
+        // Melakukan POST /products
+        createProduct({
+          name,
+          price,
+          description,
+          image,
+        });
+
+        toast({
+          title: "Product added",
+          status: "success",
+        });
+      }
+
+      handleReset(true)
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Name Produk wajib diisi").min(3, "Minimal 3 karakter").max(50, "Maksimal 50 karakter"),
+      price: Yup.number().required("Harga wajib diisi").min(10000, "Minimal Rp10.000"),
+      description: Yup.string().nullable(),
+      image: Yup.string().required().matches(/^(https?:\/\/[^\s]+(\.png|\.jpg|\.jpeg|\.gif)(\?[^#]*)?(#.*)?)$/, "Masukkan URL gambar yang valid"),
+    }),
+  });
+
+  const { mutate: createProduct, isLoading: createProductsIsLoading } =
+    useCreateProduct({
+      onSuccess: () => {
+        refetchProducts();
+      },
+    });
+
+  const { mutate: deleteProduct } = useDeleteProduct({
+    onSuccess: () => {
+      refetchProducts();
+    },
+  });
+
+  const { mutate: editProduct, isLoading: editProductIsLoading } =
+    useEditProduct({
+      onSuccess: () => {
+        refetchProducts();
+      },
+    });
+
+  const handleFormInput = (event) => {
+    formik.setFieldValue(event.target.name, event.target.value);
+  };
+
+  const confirmationDelete = (productId) => {
+    const shouldDelete = confirm("Are you sure?");
+
+    if (shouldDelete) {
+      deleteProduct(productId);
+      toast({
+        title: "Deleted product",
+        status: "info",
+      });
+    }
+  };
+
+  const handleReset = (closeEdit) => {
+    formik.setFieldValue("name", "");
+    formik.setFieldValue("price", "");
+    formik.setFieldValue("description", "");
+    formik.setFieldValue("image", "");
+    if (closeEdit) {
+      formik.setFieldValue("id", "");
+    }
+
+  }
+
+  const onEditClick = (product) => {
+    formik.setFieldValue("id", product.id);
+    formik.setFieldValue("name", product.name);
+    formik.setFieldValue("description", product.description);
+    formik.setFieldValue("price", product.price);
+    formik.setFieldValue("image", product.image);
+  };
+
   return (
     <>
       <Head>
@@ -14,101 +162,110 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <Container my={5} maxW={"900px"}>
+          <Heading textAlign={"center"}>Home Page</Heading>
+          <form onSubmit={formik.handleSubmit}>
+            <VStack my={5} spacing="3">
+              <FormControl isInvalid={formik.errors.name && formik.touched.name}>
+                <FormLabel>Nama Produk</FormLabel>
+                <Input
+                  placeholder="Masukan Nama Produk"
+                  onChange={handleFormInput}
+                  name="name"
+                  value={formik.values.name}
+                  _focusVisible={formik.errors.name ? {} : null}
+                />
+                <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={formik.errors.price && formik.touched.price}>
+                <FormLabel>Harga</FormLabel>
+                <Input
+                  placeholder="Masukan Harga Produk"
+                  onChange={handleFormInput}
+                  name="price"
+                  value={formik.values.price}
+                  _focusVisible={formik.errors.price ? {} : null}
+                />
+                <FormErrorMessage>{formik.errors.price}</FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Deskripsi <span style={{ color: "gray", fontSize: "12px" }}>(opsional)</span></FormLabel>
+                <Input
+                  placeholder="Masukan Deskripsi produk"
+                  onChange={handleFormInput}
+                  name="description"
+                  value={formik.values.description}
+                />
+              </FormControl>
+              <FormControl isInvalid={formik.errors.image && formik.touched.image}>
+                <FormLabel>Gambar</FormLabel>
+                <Input
+                  placeholder="Masukan Gambar Produk"
+                  onChange={handleFormInput}
+                  name="image"
+                  value={formik.values.image}
+                  _focusVisible={formik.errors.image ? {} : null}
+                />
+                <FormErrorMessage>{formik.errors.image}</FormErrorMessage>
+              </FormControl>
+              {createProductsIsLoading || editProductIsLoading ? (
+                <Spinner />
+              ) : (
+                <HStack>
+                  <Button name="id" value={formik.values.id} type="submit" colorScheme={formik.values.id ? "blue" : "green"}>{formik.values.id ? "Edit" : "Submit"} Product</Button>
+                  <Button type="reset" onClick={() => handleReset()}>Reset</Button>
+                  {formik.values.id && <Button type="button" colorScheme={"red"} onClick={() => handleReset(true)}>Batal Edit</Button>}
+                </HStack>
+              )}
+            </VStack>
+          </form>
+          <TableContainer my="10">
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>ID</Th>
+                  <Th>Name</Th>
+                  <Th>Price</Th>
+                  <Th>Description</Th>
+                  <Th>Action</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <RenderProducts data={data} onEditClick={onEditClick} confirmationDelete={confirmationDelete} />
+                {productsIsLoading && <Tr><Td textAlign={"center"} colSpan={5}><Spinner /></Td></Tr>}
+                {!data && !productsIsLoading && <Tr><Td textAlign={"center"} fontWeight={"bold"} color={"red"} colSpan={5}>No data</Td></Tr>}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Container>
       </main>
     </>
   );
 }
+
+const RenderProducts = ({ data, onEditClick, confirmationDelete }) => {
+  return data?.data.data.map((product) => {
+    return (
+      <Tr key={product.id}>
+        <Td>{product.id}</Td>
+        <Td>{product.name}</Td>
+        <Td>{product.price}</Td>
+        <Td>{product.description}</Td>
+        <Td>
+          <HStack>
+            <Button size={"sm"} onClick={() => onEditClick(product)} colorScheme="blue">
+              Edit
+            </Button>
+            <Button size={"sm"}
+              onClick={() => confirmationDelete(product.id)}
+              colorScheme="red"
+            >
+              Delete
+            </Button>
+          </HStack>
+        </Td>
+      </Tr>
+    );
+  });
+};
